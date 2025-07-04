b780e6598435f5ca14335c2daf250e57babd20ac
Tarun Jeevan <tjeevan200@gmail.com>
2025-07-04 09:35:43 -0500

    Complete project setup

---
+++ README.md
@@ -0,0 +1,12 @@
+# Personal Calendar App
+
+## Features
+- [ ] 
+
+
+## Improvements
+- [ ] Add delayed event activation (set a date and time for an event to become public)
+- [ ] Look into adding other calendar integrations such as Outlook calendar, etc.
+- [ ] Add user customization features (e.g., dashboard layout, font family/size, color theming, etc.)
+- [ ] Event searching and sorting via event names, tags, etc.
+- [ ] Allow non-users to create events for a registered user and send an invite for an available time slot
---
+++ app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+    title: "Kalendra",
+    description: "Kalendra is a simple and efficient calendar app that helps you manage your events, meetings, and schedulues with ease. Stay organized and never miss an important date again!",
+};
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="en">
+            <body
+                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+            >
+                {children}
+            </body>
+        </html>
+    );
+}
